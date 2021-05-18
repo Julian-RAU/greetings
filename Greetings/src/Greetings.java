4107efe7b80d9c06bbc0f3a4d75e28af3596d1b9
Julian-RAU <82039155+Julian-RAU@users.noreply.github.com>
2021-05-18 15:47:59 +0200

    Abfrage nach schönem Wetter hinzugefügt

--- Greetings/src/Greetings.java
+++ Greetings/src/Greetings.java
@@ -2,9 +2,14 @@ public class Greetings
 {
     public static void main (String[] args)
     {
+        boolean isNiceWeather = true;
         for (int i = 0; i < 10; i++) {
             System.out.println("Hello World!");
         }
         System.out.println("Hello World! wurde 10-mal ausgegeben!");
+
+        if (isNiceWeather) {
+            System.out.println("Es ist schönes Wetter!");
+        }
     }
 }
